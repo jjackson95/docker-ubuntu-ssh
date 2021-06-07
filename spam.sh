5dd01e4558a5a7c60f7bb5e008e2edce9decc1e8
Your Name <you@example.com>
2021-06-07 15:52:05 +0800

    fill bytes

---
+++ spam.sh
@@ -0,0 +1,9 @@
+touch "spamfile.txt"
+echo "spam " >> "spamfile.txt"
+git pull
+git add .
+git commit -m "fill bytes"
+git push --force origin master
+printf "tunggu 1jam"
+sleep 3600
+start spam1.sh
